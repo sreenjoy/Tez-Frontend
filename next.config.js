86907485e8f226149ea0cdb8395544bb78f1e83e
Sreenjoy Mallick <sreenjoymallick@Sreenjoys-MacBook-Air-3.local>
2025-05-16 02:36:57 +0530

    Remove deprecated swcMinify option from Next.js config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
   output: 'standalone',
   transpilePackages: ['react-chartjs-2', 'chart.js'],
   experimental: {
